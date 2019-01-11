29b43a099191b3ac65241245748b02a4a5628888
Natalia Zartha Suarez <natalia.ti@onoff.com.co>
2019-01-11 11:13:21 -0500

    Eliminar cliente

--- GestionReservas.Data/GestionReservas.Data/Repositorios/ClienteRepositorio.cs
+++ GestionReservas.Data/GestionReservas.Data/Repositorios/ClienteRepositorio.cs
@@ -20,9 +20,29 @@ namespace GestionReservas.Data.Repositorios
             RequiresContextDisposal = true;
         }
 
-        public void EliminarCliente(Cliente clientes)
+        public void EliminarCliente(int idClientes)
         {
-            throw new NotImplementedException();
+            //Validar si tiene reservaciones sin confirmar
+            Reservacion reservaciones = Context.Reservacion.Where(x => x.Confimada == false).FirstOrDefault();
+            if (reservaciones == null)
+            {
+
+                Cliente cliente = Context.Cliente.Where(x => x.Id == idClientes).FirstOrDefault();
+                if (cliente != null)
+                {
+                    //Eliminar reservaciones
+                    IEnumerable<Reservacion> reservacionesCliente = Context.Reservacion.Where(e => e.IdCliente == idClientes).ToList();
+                    foreach (var item in reservacionesCliente)
+                    {
+                        Context.Reservacion.Remove(item);
+                    }
+                    Context.SaveChanges();
+
+                    //Eliminar cliente
+                    Context.Cliente.Remove(cliente);
+                    Context.SaveChanges();
+                }
+            }
         }
 
         public Cliente GuardarActualizarCliente(Cliente clientes)
@@ -76,7 +96,7 @@ namespace GestionReservas.Data.Repositorios
 
         public IEnumerable<Cliente> ListarCliente(Cliente clientes)
         {
-           
+
             IEnumerable<Cliente> listar = Context.Cliente.ToList();
             return listar;
         }
